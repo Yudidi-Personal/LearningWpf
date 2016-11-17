47486edebb7c9a2f940481a3e10d82e769c94eef
sicnuthinkgeo <mikeyu@thinkgeo.com>
2016-11-17 17:21:46 +0800

    # StackPanel 1. Orientation # WrapPanel 1. Orientation 2. ItemHeight 3. ItemWidth * 2,3均不设置时,因为总是取Max(子元素),所以默认不会有截断发生.

---
+++ 2WpfLayout/StackPanelLayout.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _2WpfLayout
+{
+    /// <summary>
+    /// Interaction logic for StackPanel.xaml
+    /// </summary>
+    public partial class StackPanelLayout : Window
+    {
+        public StackPanelLayout()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (stackPnl.Orientation == Orientation.Horizontal)
+            {
+                stackPnl.Orientation = Orientation.Vertical;
+            }
+            else
+            {
+                stackPnl.Orientation = Orientation.Horizontal;
+            }
+        }
+
+        private void AddButtons(object sender, RoutedEventArgs e)
+        {
+            StackPanel stackPanel = new StackPanel();
+            this.Content = stackPanel;
+            stackPanel.Background = new SolidColorBrush(Colors.Azure);
+
+            stackPanel.Children.Add(new Button() { Content="Back Button one"});
+            stackPanel.Children.Add(new Button() { Content="Back Button two"});
+            stackPanel.Children.Add(new Button() { Content="Back Button three"});
+        }
+    }
+}
