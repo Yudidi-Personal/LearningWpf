037b5841afe904f456488ef46eeeeabac36a09b0
sicnuthinkgeo <mikeyu@thinkgeo.com>
2016-11-18 19:02:01 +0800

    Add readme.txt to explain projects.

--- 3Binding/Hero.cs
+++ 3Binding/Hero.cs
@@ -1,23 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace _3Binding
 {
-    public class Hero
+    public class Hero: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int id;
+        private string name;
+        private string skill;
+        private bool hasM;
+
         public Hero(int id, string name, string skill, bool hasM)
         {
-            this.Name = name;
-            this.Id = id;
-            this.Skill = skill;
-            this.HasM = hasM;
+            Name = name;
+            ID = id;
+            Skill = skill;
+            HasM = hasM;
         }
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Skill { get; set; }
-        public bool HasM { get; set; }
+
+        public int ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("ID"));
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                this.name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public string Skill
+        {
+            get { return skill; }
+            set
+            {
+                this.skill = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Skill"));
+            }
+        }
+
+        public bool HasM
+        {
+            get { return hasM; }
+            set
+            {
+                this.hasM = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Skill"));
+            }
+        }
     }
 }
---
+++ 3Binding/readme.txt
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// WPF的Binding学习笔记(二): http://www.cnblogs.com/pasoraku/archive/2012/10/25/2738428.html
+/// </summary>
+
+
+通过比较Hero设置INotifyPropertyChanged接口后,前台和后台绑定的数据是否同步,是否完成"数据驱动"
+=> 理解INotifyPropertyChanged所起的作用
