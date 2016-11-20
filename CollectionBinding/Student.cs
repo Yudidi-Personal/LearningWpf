64ae9a16de8db783721d0c9679eb0c16e74e8eb8
sicnuthinkgeo <972656027@qq.com>
2016-11-20 22:56:17 +0800

    Collection　Binding

---
+++ CollectionBinding/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionBinding
+{
+    class Student
+    {
+        public int StuNum { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
