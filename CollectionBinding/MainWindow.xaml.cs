9676d8ad895e6a4dccf117e940e6c12dfe58c2bf
sicnuthinkgeo <972656027@qq.com>
2016-11-20 23:30:58 +0800

    Collection　Binding: first requirement change.

--- CollectionBinding/MainWindow.xaml.cs
+++ CollectionBinding/MainWindow.xaml.cs
@@ -23,6 +23,30 @@ namespace CollectionBinding
         public MainWindow()
         {
             InitializeComponent();
+            SetDataSource();
         }
+
+        private void SetDataSource()
+        {
+            List<Student> studentList = new List<Student>()
+            {
+                new Student{StuNum=1, Name="Tim", Age=28},
+                new Student{StuNum=2, Name="Ma Guo", Age=25},
+                new Student{StuNum=3, Name="Yan", Age=25},
+                new Student{StuNum=4, Name="Xaiochen", Age=28},
+                new Student{StuNum=5, Name="Miao miao", Age=24},
+                new Student{StuNum=6, Name="Ma Zhen", Age=24}
+            };
+            lb.ItemsSource = studentList;
+            lb.DisplayMemberPath = "Name";
+            //需求变更
+            //lb.SelectedValuePath = "stuName";
+            //Binding binding = new Binding() { Source = lb, Path = new PropertyPath("SelectedValue") };
+            //stuNumTextBox.SetBinding(TextBox.TextProperty, binding);
+
+            this.lb.SelectedValuePath = "StuNum";
+            this.stuNumTextBox.SetBinding(TextBox.TextProperty, new Binding("SelectedValue") { Source = this.lb });
+        }
+
     }
 }
---
+++ CollectionBinding/readme.txt
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// 绑定集合: http://blog.csdn.net/fantasiax/article/details/2749292
+/// </summary>
+
+# 集合绑定的组成部分
+1. 集合
+2. Binding
+
+## 对集合有要求
+* 实现IEnumerable接口 -- 实现该接口
+
+
+
+# Q: 为什么Source是一个listbox控件
+```
+ Binding binding = new Binding() { Source = lb, Path = new PropertyPath("SelectedValue") };
+```
+A:
