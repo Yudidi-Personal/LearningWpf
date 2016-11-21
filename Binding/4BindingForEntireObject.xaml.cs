7ee11a9acd22f1aa5d73c3b5e0c23e3bd5e32d77
sicnuthinkgeo <mikeyu@thinkgeo.com>
2016-11-21 16:32:54 +0800

    1. 绑定到整个对象 <popup PlacementRectangle="20 20 200 20"  PlacementTarget="{Binding ElementName=btn2}"> 2. popup的多个属性. * PlacementTarget * PlacementRectangle

---
+++ Binding/4BindingForEntireObject.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaction logic for _4BindingForEntireObject.xaml
+    /// </summary>
+    public partial class _4BindingForEntireObject : Window
+    {
+        public _4BindingForEntireObject()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = true;
+            popup.PopupAnimation = System.Windows.Controls.Primitives.PopupAnimation.Scroll;
+        }
+
+        private void btn2Click(object sender, RoutedEventArgs e)
+        {
+            popup2.IsOpen = true;
+        }
+
+        //private void btn1_Click(object sender, RoutedEventArgs e)
+        //{
+        //    popusBottom2.IsOpen = false;
+        //    popusBottom2.IsOpen = true;
+        //    popusBottom2.PopupAnimation = System.Windows.Controls.Primitives.PopupAnimation.Slide;
+
+        //    if (popusBottom2.IsOpen == false)
+        //    {
+        //        popusBottom2.IsOpen = true;
+        //    }
+        //}
+    }
+}
